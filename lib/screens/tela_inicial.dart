5dacce59b59e4f6c48ce9bdeef4319180fe16a47
Caroline Vicente <cvicente@magnasistemas.com.br>
2023-05-25 09:20:20 -0300

    first commit

---
+++ lib/screens/tela_inicial.dart
@@ -0,0 +1,39 @@
+import 'package:alura_flutter_curso_1/components/tasks.dart';
+import 'package:alura_flutter_curso_1/data/task_inherited.dart';
+import 'package:alura_flutter_curso_1/screens/form_screen.dart';
+import 'package:flutter/material.dart';
+
+class InitialScreen extends StatefulWidget {
+  const InitialScreen({Key? key}) : super(key: key);
+
+  @override
+  State<InitialScreen> createState() => _InitialScreenState();
+}
+
+class _InitialScreenState extends State<InitialScreen> {
+  @override
+
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Flutter: Primeiros Passos'),
+        leading: Icon(Icons.add_task),
+      ),
+      body: Container(
+        color: Color.fromARGB(255, 208, 221, 237),
+        child: ListView(
+          children: TaskInherited.of(context).taskList,
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+         Navigator.push(
+          context,
+          MaterialPageRoute(builder: (context) => const FormScreen()));        
+         },
+        backgroundColor: Colors.blue[100],
+        child: const Icon(Icons.add),
+      ),
+    );
+  }
+}
